8781db22e72ae635625f92d15e60dcb1095db7b6
Nicolás Montes Aracena <Nicolas@android-1ec464843b17144.localdomain>
2017-05-31 14:07:13 -0400

    Nueva modificación que permite ingresar los síntomas que el paciente presenta y los guarda en una lista.

--- I_doc.py
+++ I_doc.py
@@ -9,8 +9,35 @@ class paciente:
 		self.sexo = sexo
 
 
-print('Hola, soy I-Doc!, por favor lee los siguientes síntomas y dime cuales son los tuyos\n')
+print('\nHola, soy I-Doc!\n')
+
+edad 	= input('¿Cúantos años tienes?\t: ')
+estatura= input('¿Cúanto mides [mts]?\t: ')
+peso 	= input('¿Cúanto pesas [kg]?\t: ')
+sexo	= input('¿Cúal es tu sexo [M/F]?\t: ')
+
+paciente1 = paciente(edad, estatura, peso, sexo)
+
+print('\nPor favor lee los siguientes síntomas y dime cuales son los tuyos, cuando hayas terminado ingresa "0":\n')
 
 print("""
-1.Dolor de cabeza. \t2.Mucosa. \t3.Dolor de estómago. \t4.Tos\n
+1.Dolor de cabeza.\n2.Mucosa.\n3.Dolor de estómago.\n4.Tos.\n5.Indigestión.\n6.Decaimiento.\n7.Frio.\n""")
+
+a = True
+sintomas_paciente = []
+
+while a == True:
+	nuevo_sintoma = input()
+	
+	if nuevo_sintoma == '0':
+		a = False
+	else:
+		sintomas_paciente.append(nuevo_sintoma)
+
+	
+
+
+
+
+
 
