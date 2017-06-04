6b83b9a105a75c584abe2cce5be7cca64fc3b66d
Nicolás Montes Aracena <Nicolas@MacBook-Pro-de-Nicolas.local>
2017-06-04 18:13:10 -0400

    Esta modificación permite que el paciente ingrese los síntomas con números y no escribiendolos.

--- I_doc.py
+++ I_doc.py
@@ -1,5 +1,5 @@
 
-class paciente:
+class Paciente:
 	"""Crea la clase paciente con sus atributos"""
 	
 	def __init__(self, edad, estatura, peso, sexo):
@@ -11,18 +11,29 @@ class paciente:
 
 print('\nHola, soy I-Doc!\n')
 
+# Datos para crear el objeto paciente.-
+# ====================================
 edad 	= input('¿Cúantos años tienes?\t: ')
 estatura= input('¿Cúanto mides [mts]?\t: ')
 peso 	= input('¿Cúanto pesas [kg]?\t: ')
 sexo	= input('¿Cúal es tu sexo [M/F]?\t: ')
 
-paciente1 = paciente(edad, estatura, peso, sexo)
 
+# Objeto perteneciente a la clase "paciente".-
+# ===========================================
+paciente1 = Paciente(edad, estatura, peso, sexo)
+
+
+# Recopilación de los síntomas.-
+# =============================
 print('\nPor favor lee los siguientes síntomas y dime cuales son los tuyos, cuando hayas terminado ingresa "0":\n')
 
 print("""
 1.Dolor de cabeza.\n2.Mucosa.\n3.Dolor de estómago.\n4.Tos.\n5.Indigestión.\n6.Decaimiento.\n7.Frio.\n""")
 
+
+
+# El siguente "while" llena la lista "sintomas_paciente" con la información recopilada.-
 a = True
 sintomas_paciente = []
 
@@ -30,14 +41,22 @@ while a == True:
 	nuevo_sintoma = input()
 	
 	if nuevo_sintoma == '0':
+		# Caso cuando se da término al ingreso de datos.
 		a = False
-	else:
-		sintomas_paciente.append(nuevo_sintoma)
-
-	
-
-
-
-
+	elif nuevo_sintoma == '1':
+		# Este y los siguientes "elif" llenan la lista según el numero ingresado por el paciente.
+		sintomas_paciente.append('Dolor de cabeza')
+	elif nuevo_sintoma == '2':
+		sintomas_paciente.append('Mucosa')
+	elif nuevo_sintoma == '3':
+		sintomas_paciente.append('Dolor de estómago')
+	elif nuevo_sintoma == '4':
+		sintomas_paciente.append('Tos')
+	elif nuevo_sintoma == '5':
+		sintomas_paciente.append('Indigestión')
+	elif nuevo_sintoma == '6':
+		sintomas_paciente.append('Decaimiento')
+	elif nuevo_sintoma == '7':
+		sintomas_paciente.append('Frio')
 
 
