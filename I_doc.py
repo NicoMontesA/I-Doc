785a3e2119375f4ecbbe7c602bd414cc2ab77836
Nicolás Montes Aracena <Nicolas@MBP-de-Nicolas.localdomain>
2017-06-12 13:24:20 -0400

    .

--- I_doc.py
+++ I_doc.py
@@ -17,7 +17,7 @@ sexo	= input('¿Cúal es tu sexo [M/F]?\t: ')
 
 # Recopilación de los síntomas.-
 # =============================
-print('\nPor favor lee los siguientes síntomas y dime cuales son los tuyos, cuando hayas terminado ingresa "0":\n')
+print('\nPor favor lee los siguientes síntomas y dime cuales son los tuyos, para ello ingresa un número y luego presiona la tecla "enter", realiza esto cuantas veces sea necesario. Cuando hayas terminado ingresa "0":\n')
 
 print("""
 1.Dolor de cabeza.\n2.Mucosa.\n3.Dolor de estómago.\n4.Tos.\n5.Indigestión.\n6.Decaimiento.\n7.Frio.\n""")
@@ -50,19 +50,22 @@ while a == True:
 	elif nuevo_sintoma == '7':
 		sintomas_paciente.append('Frio')
 	else:
-		print('Este número no es válido, por favor ingresa otro')
+		print('Este número no es válido, por favor ingresa otro.')
 
 
 
 # ASOCIACIÓN DE LOS SÍNTOMAS A LAS ENFERMEDADES INGRESADAS.-
 # =========================================================
 
+
+# Contadores de las enfermedades:
 resfrio 	= 0
 jaqueca 	= 0
 gastroenteritis = 0
 
+
 for i in sintomas_paciente:
-	
+	# Este "for" recorre los síntomas para asociarlos a alguna enfermedad, agregando un contador a cada enfermedad, dependiendo de si el sintoma corresponde o no a ella.
 	if i == 'Dolor de cabeza':
 		resfrio += 1
 		jaqueca += 1
@@ -91,28 +94,36 @@ for i in sintomas_paciente:
 print('resfrio 		=', resfrio)
 print('jaqueca 		=', jaqueca)
 print('gastroenteritis 	=', gastroenteritis)
+# ===========================================================================
 
 
 # RELACION CONTADOR ENFERMEDAD CON CANTIDAD DE SINTOMAS.-
 # ======================================================
 
-if resfrio > jaqueca & resfrio > gastroenteritis:
+# Al conocer cual es la enfermedad que tiene el mayor contador, se crea el el objeto "paciente" con ella.
+
+if resfrio > jaqueca and resfrio > gastroenteritis:
 	# Caso cuando el paciente se encuentra resfriado.
 	paciente = enf.Resfrio(edad,estatura,peso,sexo)
 	paciente.tratamiento()
+# ===========================================================================
+# BORRAR ESTE Y LOS SIGUIENTES "print", ESTÁN PARA VERIFICAR QUE LA CLASE SE INICIE DE MANERA CORRECTA.
 	print('Paciente de la clase Resfrio')
+# ===========================================================================
 
-elif jaqueca > resfrio & jaqueca > gastroenteritis:
+elif jaqueca > resfrio and jaqueca > gastroenteritis:
 	# Caso cuando el paciente se encuentra con jaqueca.
 	paciente = enf.Jaqueca(edad,estatura,peso,sexo)
 	paciente.tratamiento()
 	print('Paciente de la clase Jaqueca')
 
-elif gastroenteritis > resfrio & gastroenteritis > jaqueca:
+elif gastroenteritis > resfrio and gastroenteritis > jaqueca:
 	# Caso cuando el paciente se encuentra con gastroenteritis.
 	paciente = enf.Gastroenteritis(edad,estatura,peso,sexo) 
 	paciente.tratamiento()
 	print('Paciente de la clase Gastroenteritis')
+
 else:
-	print('Su enfermedad parece grave, porfavor diríjase a Urgencias')
+	# Caso cuando el paciente se encuentra con múltiples síntomas y no se pudo determinar con certeza a qué enfermeda específica corresponde.
+	print('\nSu enfermedad parece grave, por favor diríjase a Urgencias.\n')
 
